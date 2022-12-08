a6a476f222caf5028538c05189b6ec3985d90f2a
webkawsar <web.kawsarahmed@gmail.com>
2022-12-08 23:01:57 +0600

    fix chat input UI

--- src/components/messenger/MessengerUser.jsx
+++ src/components/messenger/MessengerUser.jsx
@@ -6,6 +6,7 @@ import {
   TextField,
   Typography
 } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import { format } from "date-fns";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -21,6 +22,30 @@ export const loader = async ({ params }) => {
   return { data, messages };
 };
 
+// MuiInputBase-root MuiFilledInput-root MuiFilledInput-underline MuiInputBase-colorPrimary MuiInputBase-formControl MuiInputBase-multiline css-1gzyby-MuiInputBase-root-MuiFilledInput-root
+
+const CustomizedTextField = styled(TextField)`
+  width: 40%;
+  :hover {
+    color: #2e8b57;
+  }
+  & .MuiInputBase-root {
+    border-radius: 30px;
+  }
+  & .css-1gzyby-MuiInputBase-root-MuiFilledInput-root:before {
+    border: none;
+  }
+  &
+    .css-1gzyby-MuiInputBase-root-MuiFilledInput-root:hover:not(
+      .Mui-disabled
+    ):before {
+    border: none;
+  }
+  & .css-1gzyby-MuiInputBase-root-MuiFilledInput-root:after {
+    border: none;
+  }
+`;
+
 const MessengerUser = () => {
   const { data, messages } = useLoaderData();
   const loggedInUser = useSelector((state) => state?.auth?.user);
@@ -35,40 +60,52 @@ const MessengerUser = () => {
       receiver: data.id,
     };
 
+    // validation
+    if(!message.text) return false;
+
     dispatch(sendMessage(message));
   };
 
-  // console.log(loggedInUser, 'loggedInUser');
-
   return (
-    <Box sx={{ maxHeight: "100vh" }}>
+    <Box sx={{ maxHeight: "100vh"}}>
       <Box sx={{ overflow: "hidden" }}>
         {messages.length ? (
           messages.map((message, index, array) => {
-            
             const sender = message?.sender?.username === loggedInUser?.username;
             const nextMsg = array[index + 1];
-            const hasReceiverNextMsg = nextMsg?.receiver?.username === loggedInUser?.username;
-            const hasSenderNextMsg = nextMsg?.sender?.username === loggedInUser?.username;
-    
+            const hasReceiverNextMsg =
+              nextMsg?.receiver?.username === loggedInUser?.username;
+            const hasSenderNextMsg =
+              nextMsg?.sender?.username === loggedInUser?.username;
+
             const prevMsg = array[index - 1];
-            const hasSenderPrevMsg = prevMsg?.sender?.username === loggedInUser?.username;
-            const hasReceiverPrevMsg = prevMsg?.receiver?.username === loggedInUser?.username;
+            const hasSenderPrevMsg =
+              prevMsg?.sender?.username === loggedInUser?.username;
+            const hasReceiverPrevMsg =
+              prevMsg?.receiver?.username === loggedInUser?.username;
 
             return (
               <Box key={message.id}>
                 {sender ? (
-                  <Box sx={{overflow: 'hidden'}}>
+                  <Box sx={{ overflow: "hidden" }}>
                     <Box
                       sx={{
                         textAlign: "right",
-                        float: 'right'
+                        float: "right",
                       }}
                     >
-                      <Typography variant="body2" sx={{ marginLeft: "5px", display: ((!hasSenderPrevMsg && hasSenderNextMsg) || (!hasSenderPrevMsg && !hasSenderNextMsg)) ? 'block' : 'none' }} >
-                        {
-                          format(new Date(message?.createdAt), 'h:m a')
-                        }
+                      <Typography
+                        variant="body2"
+                        sx={{
+                          marginLeft: "5px",
+                          display:
+                            (!hasSenderPrevMsg && hasSenderNextMsg) ||
+                            (!hasSenderPrevMsg && !hasSenderNextMsg)
+                              ? "block"
+                              : "none",
+                        }}
+                      >
+                        {format(new Date(message?.createdAt), "h:m a")}
                       </Typography>
                       <Paper
                         square
@@ -86,26 +123,33 @@ const MessengerUser = () => {
                       </Paper>
                     </Box>
                   </Box>
-                  
                 ) : (
                   <Box sx={{ display: "flex" }}>
                     <Avatar
                       sx={{
-                        visibility: ((!hasReceiverPrevMsg && hasReceiverNextMsg) || (!hasReceiverPrevMsg && !hasReceiverNextMsg))
-                          ? "visible"
-                          : "hidden",
+                        visibility:
+                          (!hasReceiverPrevMsg && hasReceiverNextMsg) ||
+                          (!hasReceiverPrevMsg && !hasReceiverNextMsg)
+                            ? "visible"
+                            : "hidden",
                       }}
-                    >{message?.sender?.firstName?.slice(0, 1)}</Avatar>
+                    >
+                      {message?.sender?.firstName?.slice(0, 1)}
+                    </Avatar>
                     <Box sx={{ marginLeft: "10px" }}>
                       <Typography
                         variant="caption"
                         sx={{
-                          display: ((!hasReceiverPrevMsg && hasReceiverNextMsg) || (!hasReceiverPrevMsg && !hasReceiverNextMsg)) ? "flex" : "none",
+                          display:
+                            (!hasReceiverPrevMsg && hasReceiverNextMsg) ||
+                            (!hasReceiverPrevMsg && !hasReceiverNextMsg)
+                              ? "flex"
+                              : "none",
                         }}
                       >
                         {message.sender.firstName},{" "}
                         <Typography variant="body2" sx={{ marginLeft: "5px" }}>
-                          {format(new Date(message?.createdAt), 'h:m a')}
+                          {format(new Date(message?.createdAt), "h:m a")}
                         </Typography>
                       </Typography>
                       <Paper
@@ -136,24 +180,35 @@ const MessengerUser = () => {
         )}
       </Box>
 
-      <Box sx={{ position: "fixed", bottom: 0 }}>
+      <Box
+        sx={{
+          position: "sticky",
+          left: 0,
+          bottom: 0,
+          backgroundColor: "white",
+          width: "100%",
+          zIndex: "99999",
+        }}
+      >
         <Box
           component="form"
           onSubmit={handleSubmit}
           noValidate
-          sx={{ display: "flex", alignItems: "center" }}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-          <TextField
+          <CustomizedTextField
             margin="normal"
             required
             id="message"
             name="message"
             variant="filled"
             label="Type a message"
-            sx={{ borderRadius: "50px 50px 50px 50px" }}
             multiline
             maxRows={9}
-            fullWidth
           />
 
           <Button type="submit" variant="contained" sx={{ ml: 2 }}>
